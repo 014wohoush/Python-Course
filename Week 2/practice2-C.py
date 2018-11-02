f9f7d2edefc46a0a05a7f24f9a46344d45f04577
enaswohoush <44284543+014wohoush@users.noreply.github.com>
2018-11-02 08:49:25 +0200

    Add files via upload

---
+++ Week 2/practice2-C.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+a=np.array([23,76,97,28,10,8,2,4,6])
+b=np.mean(a)
+c=np.std(a)
+d=np.median(a)
+print (f"The mean is: {b} \n The median is : {d}\n The std is:{c}")
+
+
+
+# In[ ]:
+
+
+
+
